0e8d5ca290f4ae6d1ee19f770b47c6c42dd0fe30
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-09-07 21:22:05 +0700

    Section 2: Knowledge Ingestion Pipeline: Part 6 - OCR-Based Image and Screenshot Ingestion

---
+++ src/test/java/org/example/advancedrag/ingestion/MultimodalIngestionServiceTest.java
@@ -0,0 +1,33 @@
+package org.example.advancedrag.ingestion;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.advancedrag.model.KnowledgeDocument;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+@Slf4j
+public class MultimodalIngestionServiceTest {
+
+    @Autowired
+    private MultimodalIngestionService multimodalIngestionService;
+
+    @Test
+    void shouldLoadMultimodalDocuments() {
+        List<KnowledgeDocument> documents =  multimodalIngestionService.loadDocuments();
+        log.info("Documents Loaded: {}", documents.size());
+
+        for (KnowledgeDocument document : documents) {
+
+            log.info("Source: {}", document.getSource());
+            log.info("Content:\n{}", document.getContent());
+            log.info("Metadata: {}", document.getMetadata());
+            log.info("----------------------------------------");
+
+        }
+
+    }
+}
