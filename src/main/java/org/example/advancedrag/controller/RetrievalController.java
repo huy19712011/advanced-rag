942d38891c3345869fdc852dfe073f93a7846983
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-09-09 07:20:22 +0700

    Section 5: Semantic Retrieval: Part 3 - Retrieval REST API

---
+++ src/main/java/org/example/advancedrag/controller/RetrievalController.java
@@ -0,0 +1,26 @@
+package org.example.advancedrag.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.advancedrag.dto.RetrievalRequest;
+import org.example.advancedrag.model.RetrievalResult;
+import org.example.advancedrag.retrieval.RetrievalService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/retrieval")
+@RequiredArgsConstructor
+public class RetrievalController {
+
+    private final RetrievalService retrievalService;
+
+    @PostMapping
+    public List<RetrievalResult> retrieve(@RequestBody RetrievalRequest request) {
+
+        return  retrievalService.retrieve(request.getQuery());
+    }
+}
---
+++ src/main/java/org/example/advancedrag/dto/RetrievalRequest.java
@@ -0,0 +1,11 @@
+package org.example.advancedrag.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RetrievalRequest {
+
+    private String query;
+}
